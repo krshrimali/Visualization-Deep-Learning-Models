7c1e8305e56eb1b4327b77c76ae22092bae2c5ca
kushashwa ravi shrimali <kushashwaravishrimali@gmail.com>
2018-03-10 12:33:33 +0530

    Added paremeters

--- CNN_Keras.py
+++ CNN_Keras.py
@@ -4,8 +4,17 @@ try:
     from keras.layers   import Convolution2D, MaxPooling2D
     from keras.utils    import np_utils
     from keras.datasets import mnist
+    from keras import backend as K
 except ImportError:
     print("Make sure keras is installed.")
+
+# hard coded parameters
+batch_size = 128
+num_classes = 10 # 0 to 9
+epochs = 12
+
+
+img_rows, img_cols = 28, 28
 
 # load MNIST data into training and test dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -13,7 +22,7 @@ except ImportError:
 # model architecture
 model = Sequential()
 
-model.add(Convolution2D(28, (1, 1), activation='relu', 
+model.add(Convolution2D(28, 1, 1, activation='relu', 
     input_shape = (1, 28, 28)))
 
 # hidden layer, no need to give input shape
@@ -21,7 +30,7 @@ model.add(Convolution2D(28, (1, 1), activation='relu',
 model.add(Convolution2D(28, (1, 1), activation='relu'))
 
 # max pooling, matrix size (2, 2)
-model.add(MaxPooling2D(pool_size = (1, 1)))
+model.add(MaxPooling2D(pool_size = (2, 2), dim_ordering="th"))
 
 # ?
 model.add(Dropout(0.25))
@@ -43,4 +52,5 @@ model.fit(x_train, y_train, batch_size = 32,
 # evaluation of model
 score = model.evaluate(x_test, y_test, verbose = 0)
 
+# accuracy score print
 print("Score: ", score)
